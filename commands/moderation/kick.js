360081765589f6d8bb1c34852e071ee9703eaceb
FUN_GAMING <sanjuyadav47150@gmail.com>
2026-05-31 05:56:27 +0530

    feat: add kick command

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+    name: "kick",
+    description: "Kick a member",
+
+    async execute(message) {
+
+        if (!message.member.permissions.has("KickMembers")) {
+            return message.reply("You do not have permission to use this command.");
+        }
+
+        const member = message.mentions.members.first();
+
+        if (!member) {
+            return message.reply("Please mention a member to kick.");
+        }
+
+        await member.kick();
+
+        message.reply(`${member.user.tag} has been kicked.`);
+    }
+};
