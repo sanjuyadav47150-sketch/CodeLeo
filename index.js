8276ecaf3f2ab17c48647a62903ed2ca6e0e80c1
FUN_GAMING <sanjuyadav47150@gmail.com>
2026-05-31 05:32:00 +0530

    feat: connect command handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const config = require("./config/config.json");
 
 const client = new Client({
     intents: [
@@ -9,8 +10,34 @@ const client = new Client({
     ]
 });
 
+require("./handlers/commandHandler")(client);
+
 client.once("ready", () => {
     console.log(`CodeLeo online as ${client.user.tag}`);
 });
 
-client.login("MTUxMDQyOTkwMjMwOTE2NzEwNA.Gk7t1p.PZokEuXqOv5W9-2T2RXG6O6dklWwkm3D4iK2BY");
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+
+    if (!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content
+        .slice(config.prefix.length)
+        .trim()
+        .split(/ +/);
+
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply("An error occurred while running this command.");
+    }
+});
+
+client.login(config.token);
