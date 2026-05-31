4907af3619671e4dcb55cfd838d14f3a7bc6ae36
FUN_GAMING <sanjuyadav47150@gmail.com>
2026-05-31 05:57:31 +0530

    feat: add ban command

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+    name: "ban",
+    description: "Ban a member",
+
+    async execute(message) {
+
+        if (!message.member.permissions.has("BanMembers")) {
+            return message.reply("You do not have permission to use this command.");
+        }
+
+        const member = message.mentions.members.first();
+
+        if (!member) {
+            return message.reply("Please mention a member to ban.");
+        }
+
+        await member.ban();
+
+        message.reply(`${member.user.tag} has been banned.`);
+    }
+};
